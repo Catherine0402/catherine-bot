cc964ccdd469b045a450d42a688c8e7cc0847263
Catherine0402 <85154191+Catherine0402@users.noreply.github.com>
2021-06-22 10:46:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,45 +12,52 @@ from linebot.models import (
 
 app = Flask(__name__)
 
+# Channel Access Token
 line_bot_api = LineBotApi('DxjD3LIA5vDUTMbRLhemnNFnYBXevqtImM4SH5OQ5CrCQjpMo0PxokDjyovrkGkW1GKbQdlc4AB3S+z1ByDWR9pXGcYsky6TM05dRWTFDNRnP0+WY9DDTdYFLkup57HsqEHJmdXQ1Pb8d9G9mgVtvgdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
 handler = WebhookHandler('660d9b02343eaed5c6465c5a6c121ab9')
 
 
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
-def callback():
-    # Get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # Get request body as text
+
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # Handle webhook body
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    
-    
-    text=event.message.text
 
+    text=event.message.text
 
     if (text=="Hi"):
         reply_text = "Hello"
-    elif(text=="妳好"):
+    elif(text=="你好"):
         reply_text = "哈囉"
     elif(text=="機器人"):
         reply_text = "叫我嗎"
-    elif(text=="aaaa"):
-        reply_text = "bbbb"
-    elif:
+    else:
         reply_text = text
-#如果非以上的選項，就會學妳說話
+#如果非以上的選項，就會學你說話
+
     message = TextSendMessage(reply_text)
     line_bot_api.reply_message(event.reply_token, message)
+
+   
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
